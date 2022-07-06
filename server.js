3fc1d006209251976d8f52704501110561057e6f
gretchesketch <gretchenjensen92@gmail.com>
2022-07-06 10:24:51 -0400

    added server file and set up database and port connection

---
+++ server.js
@@ -0,0 +1,27 @@
+// Required dependencies
+const express = require('express');
+const mongoose = require('mongoose');
+
+//port connection
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//use files in public folder
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+// mongoose database connection
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Log mongoose queries
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
